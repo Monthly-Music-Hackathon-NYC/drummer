eea17d51b253025cefeeb719f5675c49967d56a5
Junjie Chen <junjie.chen18@gmail.com>
2018-05-15 22:11:40 -0400

    chore: add a directory client and its content

---
+++ client/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './store';
+import Main from './main';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Main />
+  </Provider>,
+  document.getElementById('drum')
+);
